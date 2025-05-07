77f58c84a810907c9871892f0ca6665764b86c5d
Arsh Tiwari <dchglucknow@gmail.com>
2025-05-07 18:15:06 +0530

    feat: update app.jsx

--- annual-report-portal/src/App.jsx
+++ annual-report-portal/src/App.jsx
@@ -1,51 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Dashboard from "./pages/Dashboard";
-import AcademicPerformance from "./pages/AcademicPerformance";
-import ResearchPublications from "./pages/ResearchPublications";
-import FinancialOverview from "./pages/FinancialOverview";
-import StudentLife from "./pages/StudentLife";
-import Login from "./components/Login";
-import About from "./components/About";
-import Signup from "./components/Signup";
-import SuperAdminUpload from "./components/Super";
-import Work from "./components/Work";
-import Sidebar from "./components/report/Sidebar";
-import Navb from "./components/report/Nav"; 
-import KPISection from "./components/report/KPISection"; 
-import ChartCard from "./components/report/Chartcard"; 
-import AdminPage from "./components/Adminpage";
-import StudentPage from "./components/Studentpage";
-import DashboardView from "./components/Dashboardview";
-import WhyLumina from "./components/Why";
-import IntegrationSystem from "./components/Integration";
-import GetStartedSection from "./components/Last";
-import PDF from "./components/Pdf";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/navbar"
+import Footer from "./components/footer"
+import Dashboard from "./components/dashboard"
+import AcademicPerformance from "./pages/academic-performance"
+import ResearchPublications from "./pages/research-publications"
+import FinancialOverview from "./pages/financial-overview"
+import StudentLife from "./pages/student-life"
+import Login from "./components/login"
+import About from "./components/about"
+import Signup from "./components/signup"
+import SuperAdminUpload from "./components/super"
+import Work from "./components/work"
+import DashboardView from "./components/dashboard-view"
+import AdminPage from "./components/admin-page"
+import StudentPage from "./components/student-page"
+import WhyLumina from "./components/why-lumina"
+import IntegrationSystem from "./components/integration-system"
+import GetStartedSection from "./components/get-started-section"
+import PDF from "./components/pdf"
+import ProcessingPage from "./components/processing"
+import ReviewsCarousel from "./components/reviews-carousel"
+import FeasibilityAndViability from "./components/feasibility-and-viability"
+import BragAboutUs from "./components/brag-about-us"
+import AdminInfo from "./components/role-based-signup-details/admin-info"
+import StudentInfo from "./components/role-based-signup-details/student-info"
+import FacultyInfo from "./components/role-based-signup-details/faculty-info"
+import SuperAdminInfo from "./components/role-based-signup-details/super-admin-info"
 
-import ProcessingPage from "./components/Processing";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"
 
-
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-
-import ReviewsCarousel from "./components/Use";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import FeasibilityAndViability from "./components/Fandv";
-import BragAboutUs from "./components/Brag";
-
-import AdminInfo from "./components/Role-based-signup-details/AdminInfo";
-import StudentInfo from "./components/Role-based-signup-details/StudentInfo";
-import FacultyInfo from "./components/Role-based-signup-details/FacultyInfo";
-import SuperAdminInfo from "./components/Role-based-signup-details/SuperAdminInfo";
-
-
-ChartJS.register(ArcElement, Tooltip, Legend);
-
-
-
+ChartJS.register(ArcElement, Tooltip, Legend)
 
 const App = () => {
   return (
@@ -72,32 +56,28 @@ const App = () => {
             <Route path="/admin-info" element={<AdminInfo />} />
             <Route path="/student-info" element={<StudentInfo />} />
             <Route path="/faculty-info" element={<FacultyInfo />} />
-
-
-            <Route path = "/pdf" element = {<PDF/>}/>
-
-
+            <Route path="/pdf" element={<PDF />} />
           </Routes>
         </main>
         <Footer />
       </div>
     </Router>
-  );
-};
+  )
+}
 
 const LandingPage = () => {
   return (
     <div className="flex flex-col">
       <Dashboard />
       <About />
-      <WhyLumina/>
-      <IntegrationSystem/>
-      <ReviewsCarousel/>
-      <FeasibilityAndViability/>
-      <BragAboutUs/>
-      <GetStartedSection/>
+      <WhyLumina />
+      <IntegrationSystem />
+      <ReviewsCarousel />
+      <FeasibilityAndViability />
+      <BragAboutUs />
+      <GetStartedSection />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
